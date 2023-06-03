6828acd23c55b9718e1bc1f61d9426fb15ed9057
Aleksei Zakharov <alexey.v.zaharov@gmail.com>
2023-06-03 12:58:30 -0600

    Fix ioS

--- ios/DeviceUptime.m
+++ ios/DeviceUptime.m
@@ -9,7 +9,7 @@ RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(getUptime)
 {
   __uint64_t nsec = clock_gettime_nsec_np(CLOCK_MONOTONIC_RAW);
   NSInteger msec = nsec / 1000000;
-  return msec;
+  return @(msec);
 }
 
 @end
