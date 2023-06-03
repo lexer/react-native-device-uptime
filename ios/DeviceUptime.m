7de3c0eb4cc4ccea179a60ba5819c8b19f7db4c3
Aleksei Zakharov <alexey.v.zaharov@gmail.com>
2023-06-03 11:48:29 -0600

    make ios method sync

--- ios/DeviceUptime.m
+++ ios/DeviceUptime.m
@@ -5,7 +5,7 @@
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(getUptime:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(getUptime)
 {
   __uint64_t nsec = clock_gettime_nsec_np(CLOCK_MONOTONIC_RAW);
   NSInteger msec = nsec / 1000000;
