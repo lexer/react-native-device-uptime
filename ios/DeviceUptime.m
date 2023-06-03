17e0baf312a15e7d6fb3cf4e16efaf84cb639890
Aleksei Zakharov <alexey.v.zaharov@gmail.com>
2023-06-03 11:28:46 -0600

    Use clock_gettime_nsec_np for iOS instead

--- ios/DeviceUptime.m
+++ ios/DeviceUptime.m
@@ -7,8 +7,9 @@ RCT_EXPORT_MODULE()
 
 RCT_EXPORT_METHOD(getUptime:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 {
-  NSTimeInterval uptime = [[NSProcessInfo processInfo] systemUptime];
-  resolve([NSString stringWithFormat:@"%f", uptime]);  
+  __uint64_t nsec = clock_gettime_nsec_np(CLOCK_MONOTONIC_RAW);
+  NSInteger msec = nsec / 1000000;
+  resolve([NSString stringWithFormat:@"%ld", msec]));
 }
 
 @end
